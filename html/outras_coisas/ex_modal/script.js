f036b3a0e5e5fb0570566ff36b92d32aec4610f2
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2017-11-09 11:11:55 -0200

    Add files via upload

---
+++ html/outras_coisas/ex_modal/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".clicavel").click(function(){
+		var id;
+		id = $(this).attr('id');
+		$("."+id).removeClass('escondido')
+	})
+	$(".fechar").click(function(){
+		$(".modal").addClass('escondido');
+	})
+})
