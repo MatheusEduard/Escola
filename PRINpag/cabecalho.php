b4c02fe57827613a2cf811cf44f27b10f896b16d
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2018-03-23 16:52:22 -0300

    Add files via upload

---
+++ PRINpag/cabecalho.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/completo.css">
+	<title>ResenhasRPG</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<!-- cabeçalho -->
+	<header>
+		<img src="imagens/logo.png" id="logo"/>
+		<h1 id="nome_site">Resenhas de RPG</h1>
+	</header>
+	<div class='divider'></div>
+	<!-- menu -->
+	
+	<nav class="menu">
+		<a href=""><section class="item menu">Home</section></a>
+		<a href=""><section class="item menu">Jogos</section></a>
+		<a href=""><section class="item menu">Conquistas</section></a>
+		<a href=""><section class="item menu">Reviews</section></a>
+		<a href=""><section class="item menu ativo">Ajuda</section></a>
+		<section class="itemPesq menu"><input type="text" id="barraPesq" placeholder=" Pesquisar"></section>
+	</nav>
+	<div class='divider'></div>
+
+	<!-- colunas para centralizar -->
+	<div class="coluna10">.</div>
