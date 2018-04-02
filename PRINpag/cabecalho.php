bee1a3bea25e45bb865f16f4eeba541ff27363e0
MatheusEduard <32649329+MatheusEduard@users.noreply.github.com>
2018-04-02 14:31:34 -0300

    Add files via upload

--- PRINpag/cabecalho.php
+++ PRINpag/cabecalho.php
@@ -1,14 +1,32 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/completo.css">
-	<title>ResenhasRPG</title>
+	<title>Taverna do Dragão</title>
 	<meta charset="utf-8">
 </head>
 <body>
 	<!-- cabeçalho -->
 	<header>
 		<img src="imagens/logo.png" id="logo"/>
-		<h1 id="nome_site">Resenhas de RPG</h1>
+		<h1 id="nome_site">Taverna do Dragão</h1>
+
+
+		<form method="post" action="login.php">
+			<section id="formulario">
+				<section class="login">
+					<label id="login" for="login">Login</label>
+					<input type="text" name="login" id="tamanhobarra1" autocomplete="off">
+				</section>
+
+				<section class="senha">	
+					<label id="senha" for="senha">Senha</label>
+					<input type="password" name="senha" id="tamanhobarra2">
+				</section>
+				
+				<input id="enviar" type="submit" value="Enviar Dados">
+			</section>
+			</form>
+
 	</header>
 	<div class='divider'></div>
 	<!-- menu -->
@@ -19,7 +37,8 @@
 		<a href=""><section class="item menu">Conquistas</section></a>
 		<a href=""><section class="item menu">Reviews</section></a>
 		<a href=""><section class="item menu ativo">Ajuda</section></a>
-		<section class="itemPesq menu"><input type="text" id="barraPesq" placeholder=" Pesquisar"></section>
+		<section class="itemPesq menu"><input type="text" id="barraPesq"> <input type="button" value="Procurar" id="botao"></section>
+
 	</nav>
 	<div class='divider'></div>
 
